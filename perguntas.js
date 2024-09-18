453dcd21455d8f8549dac962a504b4e2ed183b40
FernandaGebhardt <160048473+FernandaGebhardt@users.noreply.github.com>
2024-09-18 14:30:58 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,28 +1,53 @@
 criaCartao(
     'Fatos',
-    'Quem criou a lâmpada elétrica?',
-    'Thomas Edison'
+    'Pergunta: Quem criou a lâmpada elétrica?',
+    'R: Thomas Edison'
 )
 
 criaCartao(
     'Geografia',
-    'Qual o menor país do mundo?',
-    '   Vaticano'
+    'Pergunta: Qual o menor país do mundo?',
+    'R: Vaticano'
 )
 
 criaCartao(
-    'Ingês',
-    'Qual o significado da palavra Legend?',
-    'Siginifica lenda'
+    'Ciência',
+    'Pergunta: Qual o nome da substância responsável pelo cor verde das plantas?',
+    'R: Clorofila'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a montanha mais alta do mundo?',
-    'Monte Everest'
+    'Pergunta: Qual a montanha mais alta do mundo?',
+    'R: Monte Everest'
 )
 criaCartao(
     'História',
-    'Qual inventor é conhecido como pai da aviação?',
-    'Santos Dumont'
+    'Pergunta: Qual inventor é conhecido como pai da aviação?',
+    'R: Santos Dumont'
+)
+criaCartao(
+    'Fatos',
+    'Pergunta: Qual o nome do criador do Facebook?',
+    'R: Mark Zuckerberg'
+)
+criaCartao(
+    'Arte',
+    'Pergunta: Quem pintou o quadro da Monalisa?',
+    'R: Leonardo Da Vinci'
+)
+criaCartao(
+    'Geografia',
+    'Pergunta: Em qual cidade fica a Estátua da Liberdade?',
+    'R: Nova York'
+)
+criaCartao(
+    'Mitologia',
+    'Pergunta: Na mitologia grega, Zeus era filho de quem?',
+    'R: Cronos'
+)
+criaCartao(
+    'Conhecimentos Gerais',
+    'Pergunta: Qual o nome do primeiro presidente negro eleito nos EUA?',
+    'R: Barack Obama'
 )
